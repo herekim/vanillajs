a908ad6f1a600717f12123c39c2d975665fc14bc
Kim-Heeyeol <khere220@gmail.com>
2022-02-22 20:07:26 +0900

    Add: 모멘텀 시계&인사 기능

---
+++ src/index.js
@@ -0,0 +1,1 @@
+document.body.style.backgroundImage = "url(../public/images/1.jpg)";
---
+++ src/time.js
@@ -0,0 +1,38 @@
+const clock = document.querySelector(".clock");
+const greet = document.querySelector(".greet");
+
+const handleTime = (time, num = 2) => {
+  return String(Math.floor(time)).padStart(num, "0");
+};
+
+const getHours = () => {
+  const getNow = new Date();
+  return getNow.getHours();
+};
+
+const getMinutes = () => {
+  const getNow = new Date();
+  return getNow.getMinutes();
+};
+
+const handleClock = () => {
+  return (clock.innerText = `${handleTime(getHours())}:${handleTime(
+    getMinutes()
+  )}`);
+};
+
+const handleGreetMessage = () => {
+  const isMorning = 6 < getHours() && getHours() < 12;
+  const isAfternoom = 12 < getHours() && getHours() < 18;
+
+  if (isMorning) return "morning";
+  if (isAfternoom) return "afternoon";
+  else return "evening";
+};
+
+const handleGreet = () => {
+  return (greet.innerText = `Good ${handleGreetMessage()}, here.`);
+};
+
+setInterval(handleClock, 1000);
+handleGreet();
